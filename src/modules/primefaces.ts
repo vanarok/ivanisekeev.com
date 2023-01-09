17002e071c2a40c7ba727d82ca15239a3feea064
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-01-09 22:56:40 +0600

    Primefaces: add css files to module

--- src/modules/primefaces.ts
+++ src/modules/primefaces.ts
@@ -1,4 +1,8 @@
 import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/md-light-deeppurple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
 import { type UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
