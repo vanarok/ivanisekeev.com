c05d98d3cc140f8c86dd2f95d7e4c97bb51420c0
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-01-10 14:25:54 +0600

    Gifki: add pictures

--- src/components/GifkiProject.vue
+++ src/components/GifkiProject.vue
@@ -2,18 +2,33 @@
 const gifki = ref([
   {
     id: 1,
+    description: 'Login page',
+    picture: '/gifki/gifki-login.png',
+  },
+  {
+    id: 2,
     description: 'Main page (unlogged)',
     picture: '/gifki/gifki-main-unlogged.png',
   },
   {
-    id: 2,
+    id: 3,
     description: 'Main page (logged)',
     picture: '/gifki/gifki-main-logged.png',
   },
   {
-    id: 3,
-    description: 'Login page',
-    picture: '/gifki/gifki-login.png',
+    id: 4,
+    description: 'View toggle',
+    picture: '/gifki/gifki-view-toggle.png',
+  },
+  {
+    id: 5,
+    description: 'Search autocompletion',
+    picture: '/gifki/gifki-globalsearch-autocompletion.png',
+  },
+  {
+    id: 6,
+    description: 'Global search',
+    picture: '/gifki/gifki-globalsearch.png',
   },
 ])
 
