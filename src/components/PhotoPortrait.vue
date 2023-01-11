0d732e8b2cac52452c15ef52e992fe004f1fff88
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-01-11 15:05:22 +0600

    Add lazy load for photo portait

--- src/components/PhotoPortrait.vue
+++ src/components/PhotoPortrait.vue
@@ -2,13 +2,8 @@
 
 <template>
   <v-img
-    src="/face.jpg"
+    src="/face.webp"
     class="min-w-29.1 h-30 rounded-full md:min-w-48.75 md:h-50"
-  >
-    <template #placeholder>
-      <div class="flex justify-center items-center h-full">
-        <v-progress-circular indeterminate />
-      </div>
-    </template>
-  </v-img>
+    lazy-src="/face-lazy.webp"
+  />
 </template>
