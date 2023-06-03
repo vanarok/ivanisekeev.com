7600fbc83e0cbd94f722c20efcb16fee7ad79bb4
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-06-03 16:02:54 +0600

    chore: optimize build process and add pico css

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,11 +3,19 @@ const clean = require("eleventy-plugin-clean");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(clean);
 
-  eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
-    "./node_modules/htmx.org/dist/htmx.js": "./js/htmx.js",
-    "./node_modules/@unocss/runtime/mini.global.js": "./js/unocss.js",
-  });
+  eleventyConfig.addPassthroughCopy(
+    process.env.PRODUCTION
+      ? {
+          "./node_modules/alpinejs/dist/cdn.min.js": "./js/alpine.js",
+          "./node_modules/htmx.org/dist/htmx.min.js": "./js/htmx.js",
+          "./node_modules/@picocss/pico/css/pico.min.css": "./css/pico.css",
+        }
+      : {
+          "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+          "./node_modules/htmx.org/dist/htmx.js": "./js/htmx.js",
+          "./node_modules/@picocss/pico/css/pico.css": "./css/pico.css",
+        }
+  );
 
   return {
     dir: {
