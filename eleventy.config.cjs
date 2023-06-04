5f46a6afb8e66dbc245419f7cb1a0e1bc114e1cf
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-06-04 16:35:19 +0600

    chore: add passthrough copy for public folder

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,8 @@
 const clean = require("eleventy-plugin-clean");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("public");
+
   eleventyConfig.addPlugin(clean);
 
   eleventyConfig.addPassthroughCopy(
@@ -8,22 +10,18 @@ module.exports = function (eleventyConfig) {
       ? {
           "./node_modules/alpinejs/dist/cdn.min.js": "./js/alpine.js",
           "./node_modules/htmx.org/dist/htmx.min.js": "./js/htmx.js",
-          "./src/shared/assets/vanilla.css": "./css/vanilla.css",
-          "./src/shared/assets/favicon.png": "./favicon.png",
         }
       : {
           "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
           "./node_modules/htmx.org/dist/htmx.js": "./js/htmx.js",
-          "./src/shared/assets/vanilla.css": "./css/vanilla.css",
-          "./src/shared/assets/favicon.png": "./favicon.png",
         }
   );
 
   return {
     dir: {
       input: "src",
-      includes: "widgets/includes",
-      layouts: "widgets/layouts",
+      includes: "_includes",
+      layouts: "_layouts",
     },
   };
 };
