d86831e868ff17133cd47a70cfa67fa744c5c93c
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-08-06 18:06:00 +0600

    refactor: full update

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,30 +2,14 @@ const clean = require('eleventy-plugin-clean')
 const pluginWebc = require('@11ty/eleventy-plugin-webc')
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('public')
-
     eleventyConfig.addPlugin(clean)
-
     eleventyConfig.addPlugin(pluginWebc)
-    eleventyConfig.addWatchTarget('./src/assets')
-
-    eleventyConfig.addPassthroughCopy(
-        process.env.PRODUCTION
-            ? {
-                  './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.js',
-                  './node_modules/htmx.org/dist/htmx.min.js': './js/htmx.js',
-              }
-            : {
-                  './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
-                  './node_modules/htmx.org/dist/htmx.js': './js/htmx.js',
-              },
-    )
-
+    eleventyConfig.addPassthroughCopy('public')
     return {
         dir: {
             input: 'src',
             includes: '_includes',
-            layouts: '_layouts',
+            output: 'dist',
         },
     }
 }
