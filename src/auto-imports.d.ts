0af78f8623258efc3e9b1a756a3ab31ff05a4376
Ivan Isekeev <work@ivanisekeev.com>
2024-04-08 00:09:23 +0600

    chore: update type imports

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -310,6 +310,7 @@ declare global {
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly $$: UnwrapRef<typeof import('vue/macros')['$$']>
     readonly $: UnwrapRef<typeof import('vue/macros')['$']>
@@ -608,6 +609,7 @@ declare module 'vue' {
   }
 }
 declare module '@vue/runtime-core' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly $$: UnwrapRef<typeof import('vue/macros')['$$']>
     readonly $: UnwrapRef<typeof import('vue/macros')['$']>
