0e81134f532cc2c7a931a505872b4e712135f172
Ivan Isekeev <hi@ivanisekeev.com>
2024-12-10 20:58:33 +0600

    Add project sliders

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay } from 'swiper/modules'
 
 const { t } = useI18n()
 const projects = useProjects()
+const swiper = ref(null)
 </script>
 
 <template>
@@ -27,9 +28,37 @@ const projects = useProjects()
     GitHub
   </v-btn>
 
-  <div
-    class="grid grid-cols-1 md:grid-cols-3 grid-rows-3 gap-10 mt-20 md:max-w-75vw md:mx-auto"
+  <!--  TODO: add more projects -->
+  <Swiper
+    ref="swiper"
+    :breakpoints="{ 1024: { slidesPerView: 3 } }"
+    :preload-images="false"
+    :modules="[Autoplay]"
+    :space-between="50"
+    class="w-full"
+    loop
+    :autoplay="true"
   >
+    <template v-for="project in projects" :key="project.name">
+      <SwiperSlide v-if="project.pictures.length > 0" class="cursor-grab my-20">
+        <v-img
+          :src="project.pictures[0].picture"
+          class="rounded-lg drop-shadow-2xl dark:drop-shadow-none"
+        >
+          <template #placeholder>
+            <div class="flex justify-center items-center h-full">
+              <v-progress-circular indeterminate />
+            </div>
+          </template>
+        </v-img>
+      </SwiperSlide>
+    </template>
+  </Swiper>
+
+  <div
+    class="grid grid-cols-1 md:grid-cols-3 grid-rows-3 gap-10 md:max-w-75vw md:mx-auto"
+  >
+    <!--     TODO: refactor to remove overhead of component -->
     <ProjectItem
       v-for="project in projects"
       :key="project.name"
