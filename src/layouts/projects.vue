77c8228ad5c61a7ba97c9cbf91c05184404add9a
Ivan Isekeev <hi@ivanisekeev.com>
2024-12-22 16:29:40 +0600

    Update main element in projects.vue to include padding on the x-axis.

--- src/layouts/projects.vue
+++ src/layouts/projects.vue
@@ -1,6 +1,6 @@
 <template>
   <TheAppBar />
-  <main class="py-16 text-center text-gray-600 dark:text-gray-200 md:max-w-250 m-auto">
+  <main class="py-16 px-4 text-center text-gray-600 dark:text-gray-200 md:max-w-250 m-auto">
     <RouterView />
   </main>
 </template>
